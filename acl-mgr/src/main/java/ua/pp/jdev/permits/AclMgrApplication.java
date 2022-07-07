62d446d6b1613ba871bcf8488b26dd11e4878998
zlodij <ya-zlodij@yandex.com>
2022-07-07 23:06:05 +0300

    Initial commit

---
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/AclMgrApplication.java
@@ -0,0 +1,12 @@
+package ua.pp.jdev.permits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclMgrApplication.class, args);
+	}
+}
