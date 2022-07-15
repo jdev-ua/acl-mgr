3b93e9589ac60e39a18c270d84211524ebdaf1ce
zlodij <ya-zlodij@yandex.com>
2022-07-15 20:29:22 +0300

    Extend with accessors

--- acl-mgr/src/main/java/ua/pp/jdev/permits/domain/AccessControlList.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/domain/AccessControlList.java
@@ -1,6 +1,8 @@
 package ua.pp.jdev.permits.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.constraints.NotBlank;
@@ -24,6 +26,61 @@ public class AccessControlList {
 
 	private Set<String> statuses = new HashSet<>();
 
+	private List<Accessor> accessors = new ArrayList<>();
+
+	public AccessControlList() {
+		// Add dm_owner with default setting
+		accessors.add(create("dm_owner", false, false, 7));
+		// Add dm_world with default setting
+		accessors.add(create("dm_world", false, false, 3));
+	}
+
+	private Accessor create(String name, boolean alias, boolean svc, int permit) {
+		Accessor result = new Accessor();
+		result.setName(name);
+		result.setAlias(alias);
+		result.setSvc(svc);
+		result.setPermit(permit);
+		return result;
+	}
+
+	public void setAccessors(List<Accessor> newAccessors) {
+		if(newAccessors == null) {
+			// Clear actual list
+			accessors.clear();
+			// Add dm_owner with default setting
+			accessors.add(create("dm_owner", false, false, 7));
+			// Add dm_world with default setting
+			accessors.add(create("dm_world", false, false, 3));
+		} else {
+			boolean containsOwner = false;
+			boolean containsWorld = false;
+			
+			// Check whether list of new values contains dm_world and dm_owner
+			for(Accessor accessor: newAccessors) {
+				if("dm_owner".equalsIgnoreCase(accessor.getName())) {
+					containsOwner = true;
+				}
+				if("dm_world".equalsIgnoreCase(accessor.getName())) {
+					containsWorld = true;
+				}
+			};
+			
+			// Clear actual list
+			accessors.clear();
+			// Extend list with new accessors
+			accessors.addAll(newAccessors);
+			// Add dm_owner with default setting if it is necessary
+			if(!containsOwner) {
+				accessors.add(create("dm_owner", false, false, 7));
+			}
+			// Add dm_world with default setting if it is necessary
+			if(!containsWorld) {
+				accessors.add(create("dm_world", false, false, 3));
+			}
+		}
+	}
+	
 	public void addStatus(String status) {
 		statuses.add(status);
 	}
