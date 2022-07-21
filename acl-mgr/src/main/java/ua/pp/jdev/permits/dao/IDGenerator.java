8669f5f88a0c96afe04655cf348dc73311d153d3
zlodij <ya-zlodij@yandex.com>
2022-07-21 22:41:24 +0300

    Change type of generated ID from String to Long

--- acl-mgr/src/main/java/ua/pp/jdev/permits/dao/IDGenerator.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/dao/IDGenerator.java
@@ -1,11 +1,9 @@
 package ua.pp.jdev.permits.dao;
 
+import java.util.UUID;
+
 public final class IDGenerator {
-	public final static String NULL_ID = "00000000";
-
-    private static int lastId = 0;
-
-    public synchronized static String generateID(String pattern) {
-        return String.format(pattern, ++lastId);
+    public synchronized static Long generateID() {
+    	return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
     }
 }
