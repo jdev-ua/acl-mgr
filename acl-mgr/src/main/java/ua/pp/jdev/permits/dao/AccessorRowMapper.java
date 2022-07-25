aed3374b537e8231af4608d4983dee732e6ffe18
zlodij <ya-zlodij@yandex.com>
2022-07-25 17:41:16 +0300

    Initial commit

---
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/dao/AccessorRowMapper.java
@@ -0,0 +1,52 @@
+package ua.pp.jdev.permits.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ua.pp.jdev.permits.domain.Accessor;
+
+class AccessorRowMapper implements RowMapper<Accessor> {
+	private Map<Long, Accessor> cache = new HashMap<>();
+
+	@Override
+	public Accessor mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Long id = rs.getLong("id");
+
+		Accessor accessor;
+		if (cache.containsKey(id)) {
+			accessor = cache.get(id);
+		} else {
+			accessor = new Accessor();
+
+			accessor.setId(id);
+			accessor.setName(rs.getString("name"));
+			accessor.setPermit(rs.getInt("permit"));
+			accessor.setAlias(rs.getBoolean("alias"));
+			accessor.setSvc(rs.getBoolean("svc"));
+		}
+
+		String orgLevel = rs.getString("org_level");
+		if (orgLevel != null && orgLevel.length() > 0) {
+			Set<String> orgLevels = accessor.getOrgLevels();
+			orgLevels.add(orgLevel);
+			accessor.setOrgLevels(orgLevels);
+		}
+
+		String xPermit = rs.getString("xpermit");
+		if (xPermit != null && xPermit.length() > 0) {
+			Set<String> xPermits = accessor.getXPermits();
+			xPermits.add(xPermit);
+			accessor.setXPermits(xPermits);
+		}
+		accessor.markOk();
+
+		cache.putIfAbsent(id, accessor);
+
+		return accessor;
+	}
+}
---
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/dao/AclRowMapper.java
@@ -0,0 +1,48 @@
+package ua.pp.jdev.permits.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ua.pp.jdev.permits.domain.AccessControlList;
+
+class AclRowMapper implements RowMapper<AccessControlList> {
+	private Map<Long, AccessControlList> cache = new HashMap<>();
+
+	@Override
+	public AccessControlList mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Long id = rs.getLong("id");
+
+		AccessControlList acl;
+		if (cache.containsKey(id)) {
+			acl = cache.get(id);
+		} else {
+			acl = new AccessControlList();
+			acl.setId(id);
+			acl.setName(rs.getString("name"));
+			acl.setDescription(rs.getString("description"));
+		}
+
+		String status = rs.getString("status");
+		if (status != null && status.length() > 0) {
+			Set<String> statuses = acl.getStatuses();
+			statuses.add(status);
+			acl.setStatuses(statuses);
+		}
+
+		String objType = rs.getString("obj_type");
+		if (objType != null && objType.length() > 0) {
+			Set<String> objTypes = acl.getObjTypes();
+			objTypes.add(objType);
+			acl.setObjTypes(objTypes);
+		}
+
+		cache.putIfAbsent(id, acl);
+
+		return acl;
+	}
+}
