611add93ffdb5c7bf569b8705ccace9159f562b1
zlodij <ya-zlodij@yandex.com>
2022-07-26 12:59:51 +0300

    Modify to use corresponding enum for state property

--- acl-mgr/src/main/java/ua/pp/jdev/permits/domain/Accessor.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/domain/Accessor.java
@@ -6,16 +6,20 @@ import java.util.Set;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
-import lombok.Setter;
-import lombok.AccessLevel;
+import ua.pp.jdev.permits.enums.State;
 
 @Data
 public class Accessor implements Cloneable {
 	private Long id = Long.valueOf(0);
 
 	@NotBlank(message = "{validation.notblank.name}")
+	@Length(max = 32, message = "{validation.length.name}")
+	@Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "{validation.pattern.name}")
 	private String name;
 	private boolean alias;
 	private boolean svc;
@@ -23,8 +27,7 @@ public class Accessor implements Cloneable {
 	@Max(7)
 	private int permit;
 	
-	@Setter(AccessLevel.PROTECTED)
-	private String state = "new";
+	private State state = State.NEW;
 
 	private Set<String> xPermits = new HashSet<>();
 	private Set<String> orgLevels = new HashSet<>();
@@ -35,17 +38,5 @@ public class Accessor implements Cloneable {
 		clone.xPermits = new HashSet<>(getXPermits());
 		clone.orgLevels = new HashSet<>(getOrgLevels());
 		return clone;
-	}
-	
-	public void markDeleted() {
-		setState("deleted");
-	}
-	
-	public void markDirty() {
-		setState("dirty");
-	}
-	
-	public void markOk() {
-		setState("ok");
 	}
 }
