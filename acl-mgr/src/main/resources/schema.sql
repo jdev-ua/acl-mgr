e1a123da7f58b77071b810380e8edc124b6a8c1c
zlodij <ya-zlodij@yandex.com>
2022-07-26 12:55:28 +0300

    Last changes
    - add ON DELETE CASCADE constraint for all primary keys
    - add UNIQUE constraint for acl name
    - cut down acl name's length to 32

--- acl-mgr/src/main/resources/schema.sql
+++ acl-mgr/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 /* Create table for ACL */
 create table if not exists acl (
 id identity,
-name varchar(64) not null,
+name varchar(32) not null unique,
 description varchar(255)
 );
 
@@ -12,7 +12,7 @@ acl_id bigint not null,
 obj_type varchar(32) not null
 );
 
-alter table obj_type add foreign key (acl_id) references acl(id);
+alter table obj_type add foreign key (acl_id) references acl(id) on delete cascade;
 
 /* Create table for ACL's statuses */
 create table if not exists status (
@@ -21,7 +21,7 @@ acl_id bigint not null,
 status varchar(128) not null
 );
 
-alter table status add foreign key (acl_id) references acl(id);
+alter table status add foreign key (acl_id) references acl(id) on delete cascade;
 
 /* Create table for ACL's accessors */
 create table if not exists accessor (
@@ -33,7 +33,7 @@ alias boolean,
 svc boolean
 );
 
-alter table accessor add foreign key (acl_id) references acl(id);
+alter table accessor add foreign key (acl_id) references acl(id) on delete cascade;
 
 /* Create table for accessor's org.levels */
 create table if not exists org_level (
@@ -42,7 +42,7 @@ accessor_id bigint not null,
 org_level varchar(2) not null
 );
 
-alter table org_level add foreign key (accessor_id) references accessor(id);
+alter table org_level add foreign key (accessor_id) references accessor(id) on delete cascade;
 
 /* Create table for accessor's xpermits */
 create table if not exists xpermit (
@@ -51,4 +51,4 @@ accessor_id bigint not null,
 xpermit varchar(20) not null
 );
 
-alter table org_level add foreign key (accessor_id) references accessor(id);
+alter table xpermit add foreign key (accessor_id) references accessor(id) on delete cascade;
