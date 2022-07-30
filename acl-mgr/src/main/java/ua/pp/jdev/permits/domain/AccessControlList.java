3ab974e0a82862f0f5a29a526f50b871c053ff9c
zlodij <ya-zlodij@yandex.com>
2022-07-30 17:09:38 +0300

    Modify to match Spring Data JDBC entity's requirements

--- acl-mgr/src/main/java/ua/pp/jdev/permits/domain/AccessControlList.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/domain/AccessControlList.java
@@ -1,9 +1,9 @@
 package ua.pp.jdev.permits.domain;
 
+import java.io.Serializable;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -11,13 +11,30 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import ua.pp.jdev.permits.dao.IDGenerator;
 import ua.pp.jdev.permits.enums.State;
 
 @Data
-public class AccessControlList implements Cloneable {
-	private Long id = Long.valueOf(0);
+@Table("ACL")
+public class AccessControlList implements Cloneable, Serializable, Persistable<Long> {
+	private static final long serialVersionUID = -6820394109565967621L;
+
+	@Id
+	private Long id;
+
+	@Transient
+	private State state = State.PURE;
 
 	@NotBlank(message = "{validation.notblank.name}")
 	@Length(max = 32, message = "{validation.length.name}")
@@ -27,20 +44,48 @@ public class AccessControlList implements Cloneable {
 	@Length(max = 128, message = "{validation.length.description}")
 	private String description = "";
 
-	private Set<String> objTypes = new HashSet<>();
+	@Setter(AccessLevel.PACKAGE)
+	@Getter(AccessLevel.PACKAGE)
+	@MappedCollection(idColumn = "ACL_ID")
+	private Set<Status> rawStatuses = new HashSet<>();
 
-	private Set<String> statuses = new HashSet<>();
+	@Setter(AccessLevel.PACKAGE)
+	@Getter(AccessLevel.PACKAGE)
+	@MappedCollection(idColumn = "ACL_ID")
+	private Set<ObjType> rawObjTypes = new HashSet<>();
 
-	private Map<String, Accessor> accessors = new HashMap<>();
+	@MappedCollection(idColumn = "ACL_ID")
+	private Set<Accessor> accessors = new HashSet<>();
 
 	public AccessControlList() {
+		this(State.PURE);
+	}
+
+	public AccessControlList(State state) {
+		if (State.NEW.equals(state)) {
+			setId(IDGenerator.generateID());
+		}
+
 		// Add dm_owner with default setting
 		addAccessor(create("dm_owner", false, false, 7));
 		// Add dm_world with default setting
 		addAccessor(create("dm_world", false, false, 3));
+
+		setState(state);
 	}
 
-	private Accessor create(String name, boolean alias, boolean svc, int permit) {
+	@PersistenceCreator
+	protected AccessControlList(Long id, String name, String description, Set<Status> rawStatuses,
+			Set<ObjType> rawObjTypes, Set<Accessor> accessors) {
+		setName(name);
+		setId(id);
+		setDescription(description);
+		setRawStatuses(rawStatuses);
+		setRawObjTypes(rawObjTypes);
+		setAccessors(accessors);
+	}
+
+	protected Accessor create(String name, boolean alias, boolean svc, int permit) {
 		Accessor result = new Accessor(State.NEW);
 		result.setName(name);
 		result.setAlias(alias);
@@ -52,9 +97,19 @@ public class AccessControlList implements Cloneable {
 	@Override
 	public AccessControlList clone() throws CloneNotSupportedException {
 		AccessControlList clone = (AccessControlList) super.clone();
-		clone.objTypes = new HashSet<>(getObjTypes());
-		clone.statuses = new HashSet<>(getStatuses());
-		clone.accessors = new HashMap<>();
+
+		// First untie clone from origin's status set by creation own new one
+		clone.rawObjTypes = new HashSet<>();
+		// Than populate it with values from origin
+		clone.setObjTypes(getObjTypes());
+
+		// First untie clone from origin's status set by creation own new one
+		clone.rawStatuses = new HashSet<>();
+		// Than populate it with values from origin
+		clone.setStatuses(getStatuses());
+
+		// clone.accessors = new HashMap<>();
+		clone.accessors = new HashSet<>();
 		// Deep clone contained accessors
 		clone.setAccessors(getAccessors().stream().map(accessor -> {
 			try {
@@ -68,46 +123,89 @@ public class AccessControlList implements Cloneable {
 	}
 
 	public Collection<Accessor> getAccessors() {
-		return accessors.values();
+		// Return a list of contained Accessors ordered by their names
+		return accessors.stream().sorted((o1, o2) -> o1.getName().compareTo(o2.getName())).collect(Collectors.toList());
 	}
 
 	public void setAccessors(Set<Accessor> newAccessors) {
 		if (newAccessors != null) {
-			newAccessors.forEach(accessor -> addAccessor(accessor));
+			// Remove all previous version of Accessors from ACL
+			accessors.clear();
+			// Refer new Accessors to ACL
+			Set<Accessor> temp = newAccessors.stream().peek(t -> t.setAclId(getId())).collect(Collectors.toSet());
+			// Add new Accessors to ACL
+			accessors.addAll(temp);
 		}
 	}
 
 	public void addAccessor(Accessor accessor) {
 		if (accessor != null) {
-			accessors.put(accessor.getName(), accessor);
+			// Remove previous version of Accessor from ACL
+			removeAccessor(accessor.getName());
+			// Refer new Accessor to ACL
+			accessor.setAclId(getId());
+			// Add new Accessor to ACL
+			accessors.add(accessor);
 		}
 	}
 
-	public boolean hasAccessor(String accessorName) {
-		return accessors.containsKey(accessorName);
+	public boolean hasAccessor(String name) {
+		return getAccessor(name).isPresent();
 	}
 
-	public Accessor getAccessor(String accessorName) {
-		return accessors.get(accessorName);
+	public Optional<Accessor> getAccessor(String name) {
+		return accessors.stream().filter(t -> t.getName().equalsIgnoreCase(name)).findFirst();
 	}
 
-	public void removeAccessor(String accessorName) {
-		accessors.remove(accessorName);
+	public void removeAccessor(String name) {
+		Optional<Accessor> optional = getAccessor(name);
+		if (optional.isPresent()) {
+			accessors.remove(optional.get());
+		}
 	}
 
-	public void addStatus(String status) {
-		statuses.add(status);
+	public Set<String> getStatuses() {
+		// Convert Statuses stored in internal data type into String
+		return getRawStatuses().stream().map(t -> t.getStatus()).collect(Collectors.toSet());
 	}
 
-	public void removeStatus(String status) {
-		statuses.remove(status);
+	public void setStatuses(Set<String> statuses) {
+		// Apply new not-null set of Statuses or clear current otherwise
+		if (statuses != null) {
+			// Convert new Statuses from String to internal data type before applying them
+			setRawStatuses(statuses.stream().map(t -> {
+				Status status = new Status();
+				status.setAclId(this.getId());
+				status.setStatus(t);
+				return status;
+			}).collect(Collectors.toSet()));
+		} else {
+			rawStatuses.clear();
+		}
 	}
 
-	public void addObjType(String objType) {
-		objTypes.add(objType);
+	public Set<String> getObjTypes() {
+		// Convert Obj.Types stored in internal data type into String
+		return getRawObjTypes().stream().map(t -> t.getObjType()).collect(Collectors.toSet());
 	}
 
-	public void removeObjType(String objType) {
-		objTypes.remove(objType);
+	public void setObjTypes(Set<String> objTypes) {
+		// Apply new not-null set of Obj.Types or clear current otherwise
+		if (objTypes != null) {
+			// Convert new Obj.Types from String to internal data type before applying them
+			setRawObjTypes(objTypes.stream().map(t -> {
+				ObjType objType = new ObjType();
+				objType.setAclId(this.getId());
+				objType.setObjType(t);
+				return objType;
+			}).collect(Collectors.toSet()));
+		} else {
+			rawObjTypes.clear();
+		}
+	}
+
+	@Override
+	public boolean isNew() {
+		return State.NEW.equals(getState());
 	}
 }
--- acl-mgr/src/main/java/ua/pp/jdev/permits/domain/Accessor.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/domain/Accessor.java
@@ -1,7 +1,9 @@
 package ua.pp.jdev.permits.domain;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -9,13 +11,29 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import ua.pp.jdev.permits.dao.IDGenerator;
 import ua.pp.jdev.permits.enums.State;
 
 @Data
-public class Accessor implements Cloneable {
-	private Long id = Long.valueOf(0);
+@Table("ACCESSOR")
+public class Accessor implements Cloneable, Serializable, Persistable<Long> {
+	private static final long serialVersionUID = -2252261108300139174L;
+
+	@Id
+	private Long id;
+
+	private Long aclId;
 
 	@NotBlank(message = "{validation.notblank.name}")
 	@Length(max = 32, message = "{validation.length.name}")
@@ -27,24 +45,114 @@ public class Accessor implements Cloneable {
 	@Max(7)
 	private int permit;
 
+	@Transient
 	private State state;
 
-	private Set<String> xPermits = new HashSet<>();
-	private Set<String> orgLevels = new HashSet<>();
+	@Setter(AccessLevel.PACKAGE)
+	@Getter(AccessLevel.PACKAGE)
+	@MappedCollection(idColumn = "ACCESSOR_ID")
+	private Set<OrgLevel> rawOrgLevels = new HashSet<>();
+
+	@Setter(AccessLevel.PACKAGE)
+	@Getter(AccessLevel.PACKAGE)
+	@MappedCollection(idColumn = "ACCESSOR_ID")
+	private Set<XPermit> rawXPermits = new HashSet<>();
 
 	public Accessor() {
 		this(State.PURE);
 	}
 
 	public Accessor(State state) {
+		if (State.NEW.equals(state) && getId() == null) {
+			setId(IDGenerator.generateID());
+		}
+
 		this.state = state;
+	}
+
+	@PersistenceCreator
+	public Accessor(Long id, Long aclId, String name, int permit, boolean alias, boolean svc, Set<OrgLevel> rawOrgLevels,
+			Set<XPermit> rawXPermits) {
+		setName(name);
+		setId(id);
+		setAclId(aclId);
+		setPermit(permit);
+		setAlias(alias);
+		setSvc(svc);
+		setRawOrgLevels(rawOrgLevels);
+		setRawXPermits(rawXPermits);
+		setState(State.PURE);
 	}
 
 	@Override
 	public Accessor clone() throws CloneNotSupportedException {
 		Accessor clone = (Accessor) super.clone();
-		clone.xPermits = new HashSet<>(getXPermits());
-		clone.orgLevels = new HashSet<>(getOrgLevels());
+
+		// First untie clone from origin's org.level set by creation own new one
+		clone.rawOrgLevels = new HashSet<>();
+		// Than populate it with values from origin
+		clone.setOrgLevels(getOrgLevels());
+
+		// First untie clone from origin's xPermit set by creation own new one
+		clone.rawXPermits = new HashSet<>();
+		// Than populate it with values from origin
+		clone.setXPermits(getXPermits());
+
 		return clone;
+	}
+
+	public Set<String> getOrgLevels() {
+		// Convert Org.Levels stored in internal data type into String
+		return getRawOrgLevels().stream().map(t -> t.getOrgLevel()).collect(Collectors.toSet());
+	}
+
+	public void setOrgLevels(Set<String> orgLevels) {
+		if (orgLevels != null) {
+			// Convert new Org.Levels from String to internal data type 
+			Set<OrgLevel> prepared = orgLevels.stream().map(t -> {
+				OrgLevel orgLevel = new OrgLevel();
+				orgLevel.setAccessorId(getId());
+				orgLevel.setOrgLevel(t);
+				return orgLevel;
+			}).collect(Collectors.toSet());
+			// Apply new values
+			setRawOrgLevels(prepared);
+		} else {
+			rawOrgLevels.clear();
+		}
+	}
+
+	public Set<String> getXPermits() {
+		// Convert XPermits stored in internal data type into String
+		return getRawXPermits().stream().map(t -> t.getXPermit()).collect(Collectors.toSet());
+	}
+
+	public void setXPermits(Set<String> xPermits) {
+		if (xPermits != null) {
+			// Convert new XPermits from String to internal data type
+			Set<XPermit> prepared = xPermits.stream().map(t -> {
+				XPermit xPermit = new XPermit();
+				xPermit.setAccessorId(getId());
+				xPermit.setXPermit(t);
+				return xPermit;
+			}).collect(Collectors.toSet());
+			// Apply new values
+			setRawXPermits(prepared);
+		} else {
+			rawXPermits.clear();
+		}
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+		// Update contained XPermits with new acessor's ID
+		getRawXPermits().forEach(t -> t.setAccessorId(id));
+		// Update contained Org.Levels with new acessor's ID
+		getRawOrgLevels().forEach(t -> t.setAccessorId(id));
+	}
+
+	@Override
+	public boolean isNew() {
+		return State.NEW.equals(getState());
 	}
 }
