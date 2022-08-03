b13a16d90aa59c32e28037e2058c3c22f46b25bb
zlodij <ya-zlodij@yandex.com>
2022-08-03 18:07:33 +0300

    Move to util package

--- acl-mgr/src/main/java/ua/pp/jdev/permits/dao/IDGenerator.java
+++
@@ -1,9 +0,0 @@
-package ua.pp.jdev.permits.dao;
-
-import java.util.UUID;
-
-public final class IDGenerator {
-    public synchronized static Long generateID() {
-    	return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-    }
-}
