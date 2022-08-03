6f3da436271e369f9ae2e55637d5380d53a8fdde
zlodij <ya-zlodij@yandex.com>
2022-08-03 19:22:53 +0300

    Move from domain package to data.orm.jdbc

--- acl-mgr/src/main/java/ua/pp/jdev/permits/data/orm/jdbc/TableStatus.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/data/orm/jdbc/TableStatus.java
@@ -1,17 +1,19 @@
-package ua.pp.jdev.permits.domain;
+package ua.pp.jdev.permits.data.orm.jdbc;
 
 import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Table("STATUS")
-class Status implements Serializable {
+class TableStatus implements Serializable {
 	private static final long serialVersionUID = 8071180708610099408L;
-	
+
 	@Id
 	private Long id;
 	private Long aclId;
