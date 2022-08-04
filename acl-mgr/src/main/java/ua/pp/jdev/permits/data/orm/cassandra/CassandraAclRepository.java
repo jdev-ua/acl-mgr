9fe8c5d155fdfcba40aec5ea27bae8f484463b22
zlodij <ya-zlodij@yandex.com>
2022-08-04 19:44:00 +0300

    Add @Repository annotation

--- acl-mgr/src/main/java/ua/pp/jdev/permits/data/orm/cassandra/CassandraAclRepository.java
+++ acl-mgr/src/main/java/ua/pp/jdev/permits/data/orm/cassandra/CassandraAclRepository.java
@@ -3,6 +3,8 @@ package ua.pp.jdev.permits.data.orm.cassandra;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface CassandraAclRepository extends CrudRepository<CassandraACL, UUID> {
 }
