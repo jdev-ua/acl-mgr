39befd70fbe593c604a0e11a4ec02c921b2e9807
zlodij <ya-zlodij@yandex.com>
2022-08-22 17:39:00 +0300

    Remove length and regexp validation

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Accessor.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Accessor.java
@@ -6,9 +6,6 @@ import java.util.Set;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 import ua.pp.jdev.permits.enums.State;
@@ -19,8 +16,6 @@ public class Accessor implements Cloneable {
 	private String id;
 
 	@NotBlank(message = "{validation.notblank.name}")
-	@Length(max = 32, message = "{validation.length.name}")
-	@Pattern(regexp = "^[a-zA-Z0-9-_]*$", message = "{validation.pattern.name}")
 	private String name;
 	private boolean alias;
 	private boolean svc;
