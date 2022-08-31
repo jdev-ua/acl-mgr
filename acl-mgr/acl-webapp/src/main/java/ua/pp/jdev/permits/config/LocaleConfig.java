b0523fd595d3f684bd5c6d5d099bd69e15432a27
zlodij <ya-zlodij@yandex.com>
2022-08-31 17:08:38 +0300

    Initial commit

---
+++ acl-mgr/acl-webapp/src/main/java/ua/pp/jdev/permits/config/LocaleConfig.java
@@ -0,0 +1,28 @@
+package ua.pp.jdev.permits.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(Locale.US);
+		return resolver;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+	}
+}
