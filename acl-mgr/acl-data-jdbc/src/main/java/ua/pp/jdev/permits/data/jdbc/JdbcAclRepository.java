6eaff9495258edb60a1a654cb977254823e401df
zlodij <ya-zlodij@yandex.com>
2022-09-01 01:16:03 +0300

    Swap CrudRepository parent interface on PagingAndSortingRepository to provide possibility of pagination result

--- acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jdbc/JdbcAclRepository.java
+++ acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jdbc/JdbcAclRepository.java
@@ -1,9 +1,9 @@
 package ua.pp.jdev.permits.data.jdbc;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface JdbcAclRepository extends CrudRepository<TableACL, Long> {
+interface JdbcAclRepository extends PagingAndSortingRepository<TableACL, Long> {
 
 }
--- acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jdbc/SpringDataJdbcAclDAO.java
+++ acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jdbc/SpringDataJdbcAclDAO.java
@@ -5,17 +5,21 @@ import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Lists;
 
 import lombok.extern.slf4j.Slf4j;
 import ua.pp.jdev.permits.data.AccessControlList;
-import ua.pp.jdev.permits.data.AccessControlListDAO;
+import ua.pp.jdev.permits.data.AccessControlListPageableDAO;
 
 @Slf4j
 @Component
-public class SpringDataJdbcAclDAO implements AccessControlListDAO {
+public class SpringDataJdbcAclDAO implements AccessControlListPageableDAO {
 	private JdbcAclRepository repository;
 
 	public SpringDataJdbcAclDAO(JdbcAclRepository repository) {
@@ -29,7 +33,28 @@ public class SpringDataJdbcAclDAO implements AccessControlListDAO {
 		return Lists.newArrayList(repository.findAll()).stream().map(TableACL::toAccessControlList)
 				.sorted(Comparator.comparing(AccessControlList::getName)).collect(Collectors.toList());
 	}
+	
+	public ua.pp.jdev.permits.data.Page<AccessControlList> readPage(int pageNo) {
+		Pageable pageable = PageRequest.of(pageNo, 5, Sort.by("name").ascending());
+		Page<TableACL> page = repository.findAll(pageable);
+		return new ua.pp.jdev.permits.data.Page<AccessControlList>() {
+			@Override
+			public int getPageCount() {
+				return page.getTotalPages();
+			}
 
+			@Override
+			public long getItemCount() {
+				return page.getTotalElements();
+			}
+
+			@Override
+			public Collection<AccessControlList> getContent() {
+				return page.map(TableACL::toAccessControlList).getContent();
+			}
+		};
+	}
+	
 	@Override
 	public Optional<AccessControlList> read(String id) {
 		Optional<TableACL> result = repository.findById(Long.parseLong(id));
