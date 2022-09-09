c7092564d1a32f8115be584b08e5d617820895c7
zlodij <ya-zlodij@yandex.com>
2022-09-09 23:23:40 +0300

    Changes
    - Extends from DataAccessObject interface
    - Add method 'readByName'

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/AccessControlListDAO.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/AccessControlListDAO.java
@@ -1,16 +1,7 @@
 package ua.pp.jdev.permits.data;
 
-import java.util.Collection;
 import java.util.Optional;
 
-public interface AccessControlListDAO {
-	Collection<AccessControlList> readAll();
-
-	Optional<AccessControlList> read(String id);
-
-	void create(AccessControlList acl);
-
-	void update(AccessControlList acl);
-
-	boolean delete(String id);
+public interface AccessControlListDAO extends DataAccessObject<AccessControlList> {
+	Optional<AccessControlList> readByName(String name);
 }
