6150a564cda07b241ab2dfb30e97903ab2364be6
zlodij <ya-zlodij@yandex.com>
2022-09-09 23:06:16 +0300

    Extend from DataAccessObject interface

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/UserDAO.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/UserDAO.java
@@ -1,16 +1,4 @@
 package ua.pp.jdev.permits.data;
 
-import java.util.Collection;
-import java.util.Optional;
-
-public interface UserDAO {
-	Collection<User> readAll();
-
-	Optional<User> read(String id);
-
-	void create(User acl);
-
-	void update(User acl);
-
-	boolean delete(String id);
+public interface UserDAO extends DataAccessObject<User> {
 }
