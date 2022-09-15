c8e51e14cbb67920635a4f5e28817430c88b0ca2
zlodij <ya-zlodij@yandex.com>
2022-09-16 00:57:51 +0300

    Changes
    - Remove redundant logging
    - Modify 'read' method in accordance to AccessControlList changes
    - Replace logic of 'populate' method to CommandLineRunner

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/simple/SimpleAclDAO.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/simple/SimpleAclDAO.java
@@ -8,123 +8,17 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 import ua.pp.jdev.permits.data.AccessControlList;
 import ua.pp.jdev.permits.data.AccessControlListDAO;
 import ua.pp.jdev.permits.data.Accessor;
 import ua.pp.jdev.permits.enums.State;
 import ua.pp.jdev.permits.util.IDGenerator;
 
-@Slf4j
 @Component
 public class SimpleAclDAO implements AccessControlListDAO {
 	private Map<String, AccessControlList> storage = new HashMap<>();
-
-	public SimpleAclDAO() {
-		log.info(getInitMessage());
-	}
-
-	protected String getInitMessage() {
-		return "Initializing simple ACL datasource storing data in memory";
-	}
-
-	@PostConstruct
-	protected void populate() {
-		Accessor scaner = new Accessor();
-		scaner.setName("bnk-scaner");
-		scaner.setPermit(3);
-		scaner.setAlias(true);
-		scaner.setSvc(false);
-
-		Accessor bAdmin = new Accessor();
-		bAdmin.setName("bnk_business_admin");
-		bAdmin.setPermit(6);
-		bAdmin.setAlias(false);
-		bAdmin.setSvc(false);
-		bAdmin.setXPermits(Set.of("EXECUTE_PROC", "CHANGE_LOCATION"));
-
-		Accessor tAdmin = new Accessor();
-		tAdmin.setName("bnk_tech_admin");
-		tAdmin.setPermit(7);
-		tAdmin.setAlias(false);
-		tAdmin.setSvc(false);
-		tAdmin.setXPermits(Set.of("EXECUTE_PROC", "CHANGE_LOCATION"));
-
-		Accessor pam1 = new Accessor();
-		pam1.setName("bnk_grc_pam1");
-		pam1.setPermit(3);
-		pam1.setAlias(true);
-		pam1.setSvc(false);
-		pam1.setXPermits(Set.of("EXECUTE_PROC"));
-
-		Accessor pam2 = new Accessor();
-		pam2.setName("bnk_grc_pam2");
-		pam2.setPermit(3);
-		pam2.setAlias(true);
-		pam2.setSvc(false);
-		pam2.setXPermits(Set.of("EXECUTE_PROC"));
-
-		Accessor legalPerf = new Accessor();
-		legalPerf.setName("bnk-ls-perf");
-		legalPerf.setPermit(3);
-		legalPerf.setAlias(true);
-		legalPerf.setSvc(true);
-		legalPerf.setOrgLevels(Set.of("CO", "VR", "MR", "RD"));
-
-		Accessor legalLM = new Accessor();
-		legalLM.setName("bnk-ls-lm");
-		legalLM.setPermit(3);
-		legalLM.setAlias(true);
-		legalLM.setSvc(true);
-		legalLM.setOrgLevels(Set.of("CO", "VR", "MR", "RD"));
-
-		try {
-			// Client ACL
-			AccessControlList aclClient = new AccessControlList();
-			aclClient.setName("bnk_client_acl");
-			aclClient.setDescription("Client ACL");
-			aclClient.setObjTypes(Set.of("bnk_client", "bnk_not_client"));
-			aclClient.setAccessors(Set.of(scaner.clone(), legalPerf.clone(), legalLM.clone(), pam1.clone(),
-					pam2.clone(), bAdmin.clone(), tAdmin.clone()));
-			create(aclClient);
-
-			// Committee ACL
-			AccessControlList aclCommittee = new AccessControlList();
-			aclCommittee.setName("bnk_committee_acl");
-			aclCommittee.setDescription("Credit committee ACL");
-			aclCommittee.setObjTypes(Set.of("bnk_committee"));
-			aclCommittee.setAccessors(Set.of(scaner.clone(), legalPerf.clone(), legalLM.clone(), pam1.clone(),
-					pam2.clone(), bAdmin.clone(), tAdmin.clone()));
-			create(aclCommittee);
-
-			// GRL ACL
-			AccessControlList aclGRC = new AccessControlList();
-			aclGRC.setName("bnk_grc_acl");
-			aclGRC.setDescription("Group of related companies ACL");
-			aclGRC.setObjTypes(Set.of("bnk_grc_acl"));
-			aclGRC.setAccessors(Set.of(scaner.clone(), legalPerf.clone(), legalLM.clone(), pam1.clone(), pam2.clone(),
-					bAdmin.clone(), tAdmin.clone()));
-			create(aclGRC);
-
-			// PS Conclusion ACL
-			AccessControlList aclConclPS = new AccessControlList();
-			aclConclPS.setName("bnk_concl_ps_acl");
-			aclConclPS.setDescription("Pledge service conclusion ACL");
-			aclConclPS.setObjTypes(Set.of("bnk_conclusion"));
-			aclConclPS.setStatuses(Set.of("PS_S_CO_APPROVE", "PS_S_CO_ASSIGN", "PS_S_CO_CONCL", "PS_S_CO_DONE",
-					"PS_S_CO_REJECT", "PS_S_CO_REV", "PS_S_RD_APPROVE", "PS_S_RD_ASSIGN", "PS_S_RD_CONCL",
-					"PS_S_RD_DONE", "PS_S_RD_REJECT", "PS_S_RD_REV"));
-			aclConclPS.setAccessors(Set.of(scaner.clone(), legalPerf.clone(), legalLM.clone(), pam1.clone(),
-					pam2.clone(), bAdmin.clone(), tAdmin.clone()));
-			create(aclConclPS);
-		} catch (CloneNotSupportedException cnse) {
-			throw new RuntimeException(cnse);
-		}
-	}
 
 	@Override
 	public Collection<AccessControlList> readAll() {
@@ -141,11 +35,7 @@ public class SimpleAclDAO implements AccessControlListDAO {
 		// Create a clone of current ACL to avoid unwanted changes by consumer
 		AccessControlList acl = storage.get(id);
 		if (acl != null) {
-			try {
-				result = Optional.of(acl.clone());
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
+			result = Optional.of(AccessControlList.deepCopy(acl));
 		} else {
 			result = Optional.empty();
 		}
