b673449f38072d19d7ba5afcf79c48a545eb9c3a
zlodij <ya-zlodij@yandex.com>
2022-09-16 00:46:15 +0300

    Changes
    - Add JavaDoc description for methods
    - Remove implementation of Cloneable
    - Add static methods 'softCopy' and 'deepCopy'

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Accessor.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Accessor.java
@@ -1,6 +1,7 @@
 package ua.pp.jdev.permits.data;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.validation.constraints.Max;
@@ -11,8 +12,14 @@ import lombok.Data;
 import ua.pp.jdev.permits.enums.State;
 import ua.pp.jdev.permits.util.IDGenerator;
 
+/**
+ * A domain object for Accessor of Access Control List
+ * 
+ * @author Maksym Shramko
+ *
+ */
 @Data
-public class Accessor implements Cloneable {
+public class Accessor {
 	private String id;
 
 	@NotBlank(message = "{validation.notblank.name}")
@@ -41,11 +48,44 @@ public class Accessor implements Cloneable {
 		setState(state);
 	}
 	
-	@Override
-	public Accessor clone() throws CloneNotSupportedException {
-		Accessor clone = (Accessor) super.clone();
-		clone.xPermits = new HashSet<>(getXPermits());
-		clone.orgLevels = new HashSet<>(getOrgLevels());
-		return clone;
+	private Accessor(Accessor origin, boolean clone) {
+		this(clone ? origin.getState() : State.NEW);
+		if(clone) {
+			setId(origin.getId());
+		}
+		setName(origin.getName());
+		setAlias(origin.isAlias());
+		setSvc(origin.isSvc());
+		setPermit(origin.getPermit());
+		setOrgLevels(origin.getOrgLevels());
+		setXPermits(origin.getXPermits());
+	}
+	
+	/**
+	 * Creates a new copy (has unique ID and {@code NEW} state,
+	 * all other fields are the same to origin) of the given {@code Accessor}.
+	 * The given {@code Accessor} must not be {@code null}
+	 * 
+	 * @param origin a {@code Accessor} to be copied, must be non-null
+	 * @return a new copy of the given {@code Accessor}
+	 * @throws NullPointerException if origin is {@code null}
+	 */
+	public static Accessor softCopy(Accessor origin) {
+		Objects.requireNonNull(origin);
+		return new Accessor(origin, false);
+	}
+	
+	/**
+	 * Returns a full clone (all fields, including ID and state,
+	 * are the same to origin) of the given {@code Accessor}. The given
+	 * {@code Accessor} must not be {@code null}
+	 * 
+	 * @param origin a {@code AccessControlList} to be cloned, must be non-null
+	 * @return a full clone of the given {@code Accessor}
+	 * @throws NullPointerException if origin is {@code null}
+	 */
+	public static Accessor deepCopy(Accessor origin) {
+		Objects.requireNonNull(origin);
+		return new Accessor(origin, true);
 	}
 }
