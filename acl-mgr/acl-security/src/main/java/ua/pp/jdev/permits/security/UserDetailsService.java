d7864cf457a16553e4f8e414dc56c29a629e19bc
zlodij <ya-zlodij@yandex.com>
2022-12-10 16:49:12 +0200

    Add temporary workaround for default users

--- acl-mgr/acl-security/src/main/java/ua/pp/jdev/permits/security/UserDetailsService.java
+++ acl-mgr/acl-security/src/main/java/ua/pp/jdev/permits/security/UserDetailsService.java
@@ -27,11 +27,40 @@ public class UserDetailsService implements org.springframework.security.core.use
 		String adminUsername = Role.ADMIN.getName().toLowerCase();
 		
 		Optional<UserDetailsImpl> optUser = repository.findByUsername(adminUsername);
-		// Create default administrator account if it isn't yet
+		// Create default Administrator account if it isn't yet
 		if(optUser.isEmpty()) {
 			UserDetailsImpl defaultAdmin = new UserDetailsImpl(adminUsername, encoder.encode(adminUsername),"admin@jdev.pp.ua", "", "", Role.ADMIN.getDisplay(), true);
 			defaultAdmin.grantAuthority(Role.ADMIN);
 			repository.save(defaultAdmin);
+		}
+		
+		// TODO Remove this ASAP!!!
+		
+		String viewerUsername = Role.VIEWER.getName().toLowerCase();
+		Optional<UserDetailsImpl> optViewer = repository.findByUsername(viewerUsername);
+		// Create default Viewer account if it isn't yet
+		if(optViewer.isEmpty()) {
+			UserDetailsImpl defaultViewer = new UserDetailsImpl(viewerUsername, encoder.encode(viewerUsername),"viewer@jdev.pp.ua", "", "", Role.VIEWER.getDisplay(), true);
+			defaultViewer.grantAuthority(Role.VIEWER);
+			repository.save(defaultViewer);
+		}
+		
+		String editorUsername = Role.EDITOR.getName().toLowerCase();
+		Optional<UserDetailsImpl> optEditor = repository.findByUsername(editorUsername);
+		// Create default Editor account if it isn't yet
+		if(optEditor.isEmpty()) {
+			UserDetailsImpl defaultEditor = new UserDetailsImpl(editorUsername, encoder.encode(editorUsername),"editor@jdev.pp.ua", "", "", Role.VIEWER.getDisplay(), true);
+			defaultEditor.grantAuthority(Role.EDITOR);
+			repository.save(defaultEditor);
+		}
+		
+		String editor2Username = Role.EDITOR.getName().toLowerCase() + 2;
+		Optional<UserDetailsImpl> optEditor2 = repository.findByUsername(editor2Username);
+		// Create additional Editor account if it isn't yet
+		if(optEditor2.isEmpty()) {
+			UserDetailsImpl extraEditor = new UserDetailsImpl(editor2Username, encoder.encode(editor2Username),"editor2@jdev.pp.ua", "", "", Role.VIEWER.getDisplay(), true);
+			extraEditor.grantAuthority(Role.EDITOR);
+			repository.save(extraEditor);
 		}
 	}
 
