4dfa10d4537f14992956634d73423931d1f3bad7
zlodij <ya-zlodij@yandex.com>
2022-12-15 00:05:38 +0200

    Changes
    - Remove state field
    - Use @AllArgsConstructor
    - Use builder to create Accessor

--- acl-mgr/acl-data-cassandra/src/main/java/ua/pp/jdev/permits/data/cassandra/AccessorUDT.java
+++ acl-mgr/acl-data-cassandra/src/main/java/ua/pp/jdev/permits/data/cassandra/AccessorUDT.java
@@ -1,21 +1,21 @@
 package ua.pp.jdev.permits.data.cassandra;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.springframework.data.annotation.PersistenceCreator;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.UserDefinedType;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import ua.pp.jdev.permits.data.Accessor;
-import ua.pp.jdev.permits.enums.State;
 
 @Data
+//@AllArgsConstructor
 @RequiredArgsConstructor
 @UserDefinedType("accessor")
 class AccessorUDT {
@@ -25,31 +25,11 @@ class AccessorUDT {
 	private boolean svc;
 	private int permit;
 
-	@Transient
-	private State state = State.PURE;
-
 	@Column("org_levels")
 	private Set<OrgLevelUDT> orgLevels = new HashSet<>();
 
 	@Column("xpermits")
 	private Set<XPermitUDT> xPermits = new HashSet<>();
-
-	public AccessorUDT(State state) {
-		setState(state);
-	}
-
-	@PersistenceCreator
-	public AccessorUDT(String name, int permit, boolean alias, boolean svc, Set<OrgLevelUDT> orgLevels,
-			Set<XPermitUDT> xPermits) {
-		this(State.PURE);
-
-		setName(name);
-		setPermit(permit);
-		setAlias(alias);
-		setSvc(svc);
-		setOrgLevels(orgLevels);
-		setXPermits(xPermits);
-	}
 
 	public void setOrgLevels(Set<OrgLevelUDT> newOrgLevels) {
 		orgLevels.clear();
@@ -65,38 +45,26 @@ class AccessorUDT {
 		}
 	}
 
-	public static Accessor toAccessor(AccessorUDT origin) {
-		Accessor result = null;
-
-		if (origin != null) {
-			result = new Accessor(origin.getState());
-			result.setName(origin.getName());
-			result.setPermit(origin.getPermit());
-			result.setAlias(origin.isAlias());
-			result.setSvc(origin.isSvc());
-			result.setState(origin.getState());
-			result.setOrgLevels(
-					origin.getOrgLevels().stream().map(OrgLevelUDT::getOrgLevel).collect(Collectors.toSet()));
-			result.setXPermits(origin.getXPermits().stream().map(XPermitUDT::getXpermit).collect(Collectors.toSet()));
-		}
-
-		return result;
+	public Accessor toAccessor() {
+		return Accessor.builder()
+				.name(getName())
+				.permit(getPermit())
+				.alias(isAlias())
+				.svc(isAlias())
+				.orgLevels(getOrgLevels().stream().map(OrgLevelUDT::getOrgLevel).collect(Collectors.toSet()))
+				.xPermits(getXPermits().stream().map(XPermitUDT::getXpermit).collect(Collectors.toSet()))
+				.build();
 	}
 
-	public static AccessorUDT fromAccessor(Accessor origin) {
-		AccessorUDT result = null;
+	public static AccessorUDT of(Accessor origin) {
+		Objects.requireNonNull(origin);
 
-		if (origin != null) {
-			result = new AccessorUDT(origin.getState());
-			result.setName(origin.getName());
-			result.setPermit(origin.getPermit());
-			result.setAlias(origin.isAlias());
-			result.setSvc(origin.isSvc());
-			result.setState(origin.getState());
-			result.setOrgLevels(
-					origin.getOrgLevels().stream().map(t -> new OrgLevelUDT(t)).collect(Collectors.toSet()));
-			result.setXPermits(origin.getXPermits().stream().map(t -> new XPermitUDT(t)).collect(Collectors.toSet()));
-		}
+		AccessorUDT result = new AccessorUDT(origin.getName());
+		result.setPermit(origin.getPermit());
+		result.setAlias(origin.isAlias());
+		result.setSvc(origin.isSvc());
+		result.setOrgLevels(origin.getOrgLevels().stream().map(t -> new OrgLevelUDT(t)).collect(Collectors.toSet()));
+		result.setXPermits(origin.getXPermits().stream().map(t -> new XPermitUDT(t)).collect(Collectors.toSet()));
 
 		return result;
 	}
