6f32d56e4ffe160864e409f20ae8566133c42ad9
zlodij <ya-zlodij@yandex.com>
2022-12-15 00:13:12 +0200

    Changes
    - Remove state field
    - Rename method 'fromAccessor' to 'of'
    - Make method 'toAccessor' non-static
    - Use builder to create Accessor

--- acl-mgr/acl-data-mongo/src/main/java/ua/pp/jdev/permits/data/mongo/MongoAccessor.java
+++ acl-mgr/acl-data-mongo/src/main/java/ua/pp/jdev/permits/data/mongo/MongoAccessor.java
@@ -1,14 +1,11 @@
 package ua.pp.jdev.permits.data.mongo;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
-
-import org.springframework.data.annotation.Transient;
 
 import lombok.Data;
 import ua.pp.jdev.permits.data.Accessor;
-import ua.pp.jdev.permits.enums.State;
-import ua.pp.jdev.permits.util.IDGenerator;
 
 @Data
 class MongoAccessor {
@@ -19,54 +16,30 @@ class MongoAccessor {
 	private boolean svc;
 	private int permit;
 
-	@Transient
-	private State state = State.PURE;
-
 	private Set<String> xPermits = new HashSet<>();
 	private Set<String> orgLevels = new HashSet<>();
 
-	public MongoAccessor() {
-		this(State.PURE);
+	public Accessor toAccessor() {
+		return Accessor.builder()
+				.name(getName())
+				.permit(getPermit())
+				.alias(isAlias())
+				.svc(isAlias())
+				.orgLevels(getOrgLevels())
+				.xPermits(getXPermits())
+				.build();
 	}
 
-	public MongoAccessor(State state) {
-		if (State.NEW.equals(state) && getId() == null) {
-			setId(IDGenerator.genStringID());
-		}
-
-		setState(state);
-	}
-
-	public static Accessor toAccessor(MongoAccessor origin) {
-		Accessor result = null;
-
-		if (origin != null) {
-			result = new Accessor(origin.getState());
-			result.setName(origin.getName());
-			result.setPermit(origin.getPermit());
-			result.setAlias(origin.isAlias());
-			result.setSvc(origin.isSvc());
-			result.setState(origin.getState());
-			result.setOrgLevels(origin.getOrgLevels());
-			result.setXPermits(origin.getXPermits());
-		}
-
-		return result;
-	}
-
-	public static MongoAccessor fromAccessor(Accessor origin) {
-		MongoAccessor result = null;
-
-		if (origin != null) {
-			result = new MongoAccessor(origin.getState());
-			result.setName(origin.getName());
-			result.setPermit(origin.getPermit());
-			result.setAlias(origin.isAlias());
-			result.setSvc(origin.isSvc());
-			result.setState(origin.getState());
-			result.setOrgLevels(origin.getOrgLevels());
-			result.setXPermits(origin.getXPermits());
-		}
+	public static MongoAccessor of(Accessor origin) {
+		Objects.requireNonNull(origin);
+		
+		MongoAccessor result = new MongoAccessor();
+		result.setName(origin.getName());
+		result.setPermit(origin.getPermit());
+		result.setAlias(origin.isAlias());
+		result.setSvc(origin.isSvc());
+		result.setOrgLevels(origin.getOrgLevels());
+		result.setXPermits(origin.getXPermits());
 
 		return result;
 	}
