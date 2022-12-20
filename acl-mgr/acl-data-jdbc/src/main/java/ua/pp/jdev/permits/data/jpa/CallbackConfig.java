48d6660b922d35367a434832d89b04b544ecbcfa
zlodij <ya-zlodij@yandex.com>
2022-12-20 13:07:01 +0200

    Refactor to provide proper management of child aggregates

--- acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jpa/CallbackConfig.java
+++ acl-mgr/acl-data-jdbc/src/main/java/ua/pp/jdev/permits/data/jpa/CallbackConfig.java
@@ -3,6 +3,9 @@ package ua.pp.jdev.permits.data.jpa;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -52,23 +55,53 @@ class CallbackConfig implements ApplicationContextAware {
 	@Bean
 	protected AfterSaveCallback<TableACL> aclAfterSaveCallback() {
 		return (acl) -> {
-			// Setup contained Accessors with ID of current Acl than save them
-			acl.getAccessors().forEach(accessor -> {
-				accessor.setAclId(acl.getId());
-				getAccessorRepository().save(accessor);
-			});
+			// Save Accessors
+			doomsday(
+					// Collect all Accessors for further removal
+					() -> getAccessorRepository().findAllByAclId(acl.getId()),
+					// Accessors to save
+					acl.getAccessors(),
+					// Return false to save Accessor in any case
+					skip -> false,
+					// Provide new Accessor with Acl's ID than save
+					accessor -> {
+						accessor.setAclId(acl.getId());
+						getAccessorRepository().save(accessor);
+					},
+					// Finally remove unnecessary Accessors
+					junk -> getAccessorRepository().deleteAll(junk));
 
-			// Setup contained Statuses with ID of current Acl than save them
-			acl.getObjTypes().forEach(objType -> {
-				objType.setAclId(acl.getId());
-				getObjTypeRepository().save(objType);
-			});
+			// Save Obj.Types
+			doomsday(
+					// Collect all Obj.Types for further removal
+					() -> getObjTypeRepository().findAllByAclId(acl.getId()),
+					// Obj.Types to save
+					acl.getObjTypes(),
+					// Just pass value itself 
+					Boolean::booleanValue,
+					// Provide new Obj.Type with Acl's ID than save
+					objType -> {
+						objType.setAclId(acl.getId());
+						getObjTypeRepository().save(objType);
+					},
+					// Finally remove unnecessary Obj.Types
+					junk -> getObjTypeRepository().deleteAll(junk));
 			
-			// Setup contained Statuses with ID of current Acl than save them
-			acl.getStatuses().forEach(status -> {
-				status.setAclId(acl.getId());
-				getStatusRepository().save(status);
-			});
+			// Save Statuses
+			doomsday(
+					// Collect all Statuses for further removal
+					() -> getStatusRepository().findAllByAclId(acl.getId()),
+					// Statuses to save
+					acl.getStatuses(),
+					// Just pass value itself 
+					Boolean::booleanValue,
+					// Provide new Status with Acl's ID than save
+					status -> {
+						status.setAclId(acl.getId());
+						getStatusRepository().save(status);
+					},
+					// Finally remove unnecessary Statuses
+					junk -> getStatusRepository().deleteAll(junk));
 			
 			return acl;
 		};
@@ -77,44 +110,61 @@ class CallbackConfig implements ApplicationContextAware {
 	@Bean
 	protected AfterSaveCallback<TableAccessor> accessorAfterSaveCallback() {
 		return (accessor) -> {
-			List<TableXPermit> xPermitsToDelete = new ArrayList<>();
-			// Read from repository all XPermits associated with current Accessor
-			// and place them into list of candidates to be deleted
-			getXPermitRepository().findAllByAccessorId(accessor.getId()).forEach(xPermitsToDelete::add);
+			// Save XPermits
+			doomsday(
+					// Collect all XPermits for further removal
+					() -> getXPermitRepository().findAllByAccessorId(accessor.getId()),
+					// XPermits to save
+					accessor.getXPermits(),
+					// Just pass value itself 
+					Boolean::booleanValue,
+					// Provide new XPermit with Accessor's ID than save
+					xPermit -> {
+						xPermit.setAccessorId(accessor.getId());
+						getXPermitRepository().save(xPermit);
+						},
+					// Finally remove unnecessary XPermits
+					junk -> getXPermitRepository().deleteAll(junk));
 			
-			// Save XPermits that is not currently in repository
-			accessor.getXPermits().forEach(xPermit -> {
-				// Remove XPermit that still in use from deletion list and skip saving it again into repository
-				boolean skip = xPermitsToDelete.removeIf(t -> t.getXPermit().equalsIgnoreCase(xPermit.getXPermit()));
-				if(!skip) {
-					// Setup new XPermits with ID of current Accessor than save them
-					xPermit.setAccessorId(accessor.getId());
-					getXPermitRepository().save(xPermit);
-				}
-			});
-			// Remove all unused XPermits from repository
-			getXPermitRepository().deleteAll(xPermitsToDelete);
-			
-			List<TableOrgLevel> orgLevelsToDelete = new ArrayList<>();
-			// Read from repository all Org.Levels associated with current Accessor
-			// and place them into list of candidates to be deleted
-			getOrgLevelRepository().findAllByAccessorId(accessor.getId()).forEach(orgLevelsToDelete::add);
-			
-			// Save Org.Levels that is not currently in repository
-			accessor.getOrgLevels().forEach(orgLevel -> {
-				// Remove Org.Level that still in use from deletion list and skip saving it again into repository
-				boolean skip = orgLevelsToDelete.removeIf(t -> t.getOrgLevel().equalsIgnoreCase(orgLevel.getOrgLevel()));
-				if (!skip) {
-					// Setup new Org.Level with ID of current Accessor than save them
-					orgLevel.setAccessorId(accessor.getId());
-					getOrgLevelRepository().save(orgLevel);
-				}
-			});
-			// Remove all unused Org.Levels from repository
-			getOrgLevelRepository().deleteAll(orgLevelsToDelete);			
+			// Save Org.Levels
+			doomsday(
+					// Collect all Org.Levels for further removal
+					() -> getOrgLevelRepository().findAllByAccessorId(accessor.getId()),
+					// Org.Levels to save
+					accessor.getOrgLevels(),
+					// Just pass value itself 
+					Boolean::booleanValue,
+					// Provide new Org.Level with Accessor's ID than save
+					orgLevel -> {
+						orgLevel.setAccessorId(accessor.getId());
+						getOrgLevelRepository().save(orgLevel);
+						},
+					// Finally remove unnecessary Org.Levels
+					junk -> getOrgLevelRepository().deleteAll(junk));
 			
 			return accessor;
 		};
+	}
+	
+	private <T> void doomsday(Supplier<Iterable<T>> doomsdayBook, Set<T> survivors, Predicate<Boolean> skipJudgment, Consumer<T> savior, Consumer<List<T>> annihilator) {
+		List<T> doomed = new ArrayList<>();
+		// Populate list of candidates for removal from supplier
+		doomsdayBook.get().forEach(doomed::add);
+
+		// Process required values 
+		survivors.forEach(entity -> {
+			// Remove current value from the list for removal with fixing result.
+			// True means that value already exists in repository so there is no sense to save it again.
+			boolean skipAdvice = doomed.remove(entity);
+			// Make final decision whether save is required or not
+			if (!skipJudgment.test(skipAdvice)) {
+				// Pass value to the corresponding consumer for saving. 
+				savior.accept(entity);
+			}
+		});
+
+		// Pass remaining values for removal
+		annihilator.accept(doomed);
 	}
 	
 	@Bean
@@ -152,7 +202,7 @@ class CallbackConfig implements ApplicationContextAware {
 			
 			// Map corresponding Org.Levels
 			Set<TableOrgLevel> orgLevels = Sets.newHashSet(getOrgLevelRepository().findAllByAccessorId(accessor.getId()));
-			accessor.setOrgLevels(orgLevels); 
+			accessor.setOrgLevels(orgLevels);
 			
 			return accessor;
 		};
