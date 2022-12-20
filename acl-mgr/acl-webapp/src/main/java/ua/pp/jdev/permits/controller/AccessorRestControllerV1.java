b04f796698bc606a870d854082d366ab81740f4d
zlodij <ya-zlodij@yandex.com>
2022-12-20 16:25:59 +0200

    Changes
    - Add version tag to class name
    - Extends from BaseAccessorRestController

--- acl-mgr/acl-webapp/src/main/java/ua/pp/jdev/permits/controller/AccessorRestControllerV1.java
+++ acl-mgr/acl-webapp/src/main/java/ua/pp/jdev/permits/controller/AccessorRestControllerV1.java
@@ -2,12 +2,10 @@ package ua.pp.jdev.permits.controller;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,54 +19,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import ua.pp.jdev.permits.data.Accessor;
-import ua.pp.jdev.permits.data.Acl;
-import ua.pp.jdev.permits.data.AclDAO;
-import ua.pp.jdev.permits.exception.ResourceNotFoundException;
 
 @Slf4j
 @RestController
+@Api(tags = {"accessors"})
 @RequestMapping(path = "/api/v1/acls/{aclId}/accessors", produces = { "application/json" })
-public class AccessorRestController {
-	private AclDAO aclDAO;
-
-	@Autowired
-	private void setAclDAO(AclDAO aclDAO) {
-		this.aclDAO = aclDAO;
-	}
-	
-	private Acl findAclOrThrowException(String aclId) {
-		Objects.requireNonNull(aclId);
-		
-		Optional<Acl> optAcl = aclDAO.read(aclId);
-		if (optAcl.isEmpty()) {
-			throw new ResourceNotFoundException("ACL with ID '" + aclId + "' doesn't exist!");
-		}
-		
-		return optAcl.get();
-	}
-
-	private Optional<Accessor> findAccessor(Acl acl, String accessorId) {
-		Objects.requireNonNull(acl);
-		Objects.requireNonNull(accessorId);
-		
-		Optional<Accessor> optAccessor = acl.getAccessors().stream()
-				.filter(t -> accessorId.equalsIgnoreCase(t.getId()))
-				.findAny();
-		
-		return optAccessor;
-	}
-	
+public class AccessorRestControllerV1 extends BaseAccessorRestController {
 	@ApiOperation(response = Accessor.class, value = "Retrieves all Accessors of the ACL", responseContainer = "List")
 	@GetMapping()
 	public ResponseEntity<List<Accessor>> getAllAccessors(@PathVariable String aclId) {
 		log.debug("Start getting a list of all Accessors for ACL (id='{}')", aclId);
 
-		return new ResponseEntity<List<Accessor>>(List.copyOf(findAclOrThrowException(aclId).getAccessors()), HttpStatus.OK);
+		return new ResponseEntity<>(doGetAll(aclId), HttpStatus.OK);
 	}
 
 	@ApiOperation(response = Accessor.class, value = "Retrieves an Accessor of the ACL")
@@ -80,12 +48,12 @@ public class AccessorRestController {
 			@PathVariable("accessorId") String accessorId) {
 		log.debug("Start getting an Accessor (id='{}') for ACL (id='{}')", accessorId, aclId);
 
-		Optional<Accessor> optAccessor = findAccessor(findAclOrThrowException(aclId), accessorId);
+		Optional<Accessor> optAccessor = doGet(aclId, accessorId);
 		
 		log.debug("Result of getting an Accessor: {}", optAccessor.orElse(null));
 
 		return (optAccessor.isPresent()) ?
-				new ResponseEntity<Accessor>(optAccessor.get(), HttpStatus.OK) : 
+				new ResponseEntity<>(optAccessor.get(), HttpStatus.OK) : 
 					new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
 
@@ -95,31 +63,19 @@ public class AccessorRestController {
 	public ResponseEntity<String> addAccessor(@PathVariable String aclId, @Valid @RequestBody Accessor data) {
 		log.debug("Start creating new Accessor for ACL (id='{}'): ", aclId, data);
 
-		// Get ACL that may contain specified Accessor
-		Acl acl = findAclOrThrowException(aclId);
-		
-		// Add Accessor to ACL
-		acl.addAccessor(data);
-		// Apply changes and actualize ACL
-		acl = aclDAO.update(acl);
-		// Get newly created Accessor
-		Optional<Accessor> optAccessor = acl.getAccessor(data.getName());
-		// Throw corresponding exception if ACL doesn't contain this new Accessor
-		if (optAccessor.isEmpty()) {
-			throw new RuntimeException("Failed to add new Accessor to the ACL (id='" + aclId + "')");
-		}
+		Accessor accessor = doAdd(aclId, data);
 
-		log.debug("New Accessor created sucessfully: {}", optAccessor.get());
+		log.debug("New Accessor created sucessfully: {}", accessor);
 
 		// Create Accessor's URI and place it to response headers
 		HttpHeaders responseHeaders = new HttpHeaders();
 		URI newAccessorURI = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{accessorId}")
-				.buildAndExpand(optAccessor.get().getId())
+				.buildAndExpand(accessor.getId())
 				.toUri();
 		responseHeaders.setLocation(newAccessorURI);
 
-		return new ResponseEntity<>(optAccessor.get().getId(), responseHeaders, HttpStatus.CREATED);
+		return new ResponseEntity<>(accessor.getId(), responseHeaders, HttpStatus.CREATED);
 	}
 
 	@ApiOperation(response = Void.class, value = "Updates an Accessor of the ACL")
@@ -128,29 +84,9 @@ public class AccessorRestController {
 			@Valid @RequestBody Accessor data) {
 		log.debug("Start updating an Accessor (id='{}') for ACL (id='{}'): ", accessorId, aclId, data);
 		
-		// Get ACL that may contain specified Accessor
-		Acl acl = findAclOrThrowException(aclId);
-		// Get specified Accessor from the ACL
-		Optional<Accessor> optAccessor = findAccessor(acl, accessorId);
-		// Throw corresponding exception if ACL doesn't contain specified Accessor
-		if (optAccessor.isEmpty() && acl.hasAccessor(data.getName())) {
-			throw new ResourceNotFoundException(
-					"ACL with ID '" + aclId + "' doesn't contain Accessor with ID '" + accessorId + "'!");
-		}
+		Accessor accessor = doUpdate(aclId, accessorId, data);
 		
-		// Add Accessor to ACL
-		acl.addAccessor(data);
-		// Apply changes and actualize ACL
-		acl = aclDAO.update(acl);
-		// Get newly created Accessor
-		optAccessor = acl.getAccessor(data.getName());
-		// Throw corresponding exception if ACL doesn't contain Accessor
-		if (optAccessor.isEmpty()) {
-			throw new RuntimeException("Failed to update specified Accessor (id='"
-					+ accessorId + "') from the ACL (id='" + aclId + "')");
-		}
-
-		log.debug("Accessor updated sucessfully: {}", optAccessor.get());		
+		log.debug("Accessor updated sucessfully: {}", accessor);
 
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -160,25 +96,7 @@ public class AccessorRestController {
 	public ResponseEntity<?> deleteAccessor(@PathVariable String aclId, @PathVariable String accessorId) {
 		log.debug("Start deleting an Accessor from the ACL (id='{}'): ", aclId, accessorId);
 
-		// Try to get an ACL that may contain specified Accessor
-		Acl acl = findAclOrThrowException(aclId);
-		// Get specified Accessor from the ACL
-		Optional<Accessor> optAccessor = findAccessor(acl, accessorId);
-		// Try to remove specified Accessor if the ACL contains it or do nothing otherwise 
-		if (optAccessor.isPresent()) {
-			acl.removeAccessor(optAccessor.get().getName());
-
-			// Apply changes and actualize ACL
-			acl = aclDAO.update(acl);
-			
-			// Try to get Accessor from the ACL
-			optAccessor = acl.getAccessor(optAccessor.get().getName());
-			// Throw corresponding exception if the ACL still contains specified Accessor
-			if (optAccessor.isPresent()) {
-				throw new RuntimeException("Failed to delete specified Accessor (id='"
-				+ accessorId + "') from the ACL (id='" + aclId + "')");
-			}
-
+		if (doDelete(aclId, accessorId)) {
 			log.debug("Accessor (id='{}') deleted sucessfully from the ACL (id='{}')", accessorId, aclId);
 		} else {
 			log.debug("ACL (id='{}') doesn't contains specified Accessor (id='{}')", aclId, accessorId);
