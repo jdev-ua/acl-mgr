04b73998b9150215cd3944b592ac21be07431b84
zlodij <ya-zlodij@yandex.com>
2022-12-26 01:46:14 +0200

    Modify to provide information of page number and page size

--- acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Page.java
+++ acl-mgr/acl-common/src/main/java/ua/pp/jdev/permits/data/Page.java
@@ -14,6 +14,20 @@ import java.util.Objects;
  */
 public interface Page<T> {
 	/**
+	 * Returns the number of the current {@link Page}. Is always non-negative.
+	 *
+	 * @return the number of the current {@link Page}.
+	 */
+	int getNumber();
+
+	/**
+	 * Returns the size of the {@link Page}.
+	 *
+	 * @return the size of the {@link Page}.
+	 */
+	int getSize();
+	
+	/**
 	 * Returns the number of total pages.
 	 *
 	 * @return the number of total pages
@@ -48,6 +62,38 @@ public interface Page<T> {
 	 */
 	static <T> Page<T> of(Collection<T> data, int pageNo, int pageSize) {
 		Objects.requireNonNull(data);
+		
+		if (pageNo < 0) {
+			throw new IllegalArgumentException("Page number must not be negative");
+		}
+
+		if (pageSize < 1) {
+			throw new IllegalArgumentException("Page size must be greater than 0");
+		}
+		
+		List<T> pageContent = data.stream().skip(pageNo * pageSize).limit(pageSize).toList();
+
+		return of(pageContent, pageNo, pageSize, data.size(),
+				pageSize == 0 ? 1 : (int) Math.ceil((double) data.size() / (double) pageSize));
+	}
+
+	/**
+	 * Creates a new {@link Page}
+	 * 
+	 * @param data      a collection of items to create page, must not be
+	 *                  {@code null}.
+	 * @param pageNo    zero-based page index, must not be negative.
+	 * @param pageSize  the size of the page to be returned, must be greater than 0.
+	 * @param itemCount a total amount of items, must not be negative.
+	 * @param pageCount a number of total pages, must be greater than 0.
+	 * @return a new {@code Page}
+	 * @throws {@link NullPointerException} if {@code pageContent} is {@code null};
+	 *                {@link IllegalArgumentException} if page index is negative, or
+	 *                page size less than 1, or amount of items is negative, or
+	 *                number of total pages less than 1
+	 */
+	static <T> Page<T> of(Collection<T> pageContent, int pageNo, int pageSize, int itemCount, int pageCount) {
+		Objects.requireNonNull(pageContent);
 
 		if (pageNo < 0) {
 			throw new IllegalArgumentException("Page number must not be negative");
@@ -57,22 +103,38 @@ public interface Page<T> {
 			throw new IllegalArgumentException("Page size must be greater than 0");
 		}
 
-		List<T> pageContent = data.stream().skip(pageNo * pageSize).limit(pageSize).toList();
-
+		if (itemCount < 0) {
+			throw new IllegalArgumentException("Item count must not be negative");
+		}
+		
+		if (pageCount < 1) {
+			throw new IllegalArgumentException("Page size must be greater than 0");
+		}
+		
 		return new Page<T>() {
 			@Override
 			public int getPageCount() {
-				return pageSize == 0 ? 1 : (int) Math.ceil((double) data.size() / (double) pageSize);
+				return pageCount;
 			}
 
 			@Override
 			public long getItemCount() {
-				return data.size();
+				return itemCount;
 			}
 
 			@Override
 			public Collection<T> getContent() {
 				return pageContent;
+			}
+
+			@Override
+			public int getNumber() {
+				return pageNo;
+			}
+
+			@Override
+			public int getSize() {
+				return pageSize;
 			}
 		};
 	}
